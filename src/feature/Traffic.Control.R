7fb87d0131ab67fdf7224ef74683073432707abf
Donald Dong <xdong@csumb.edu>
2018-03-27 10:03:11 -0700

    processed Traffic.Control

--- src/feature/Traffic.Control.R
+++ src/feature/Traffic.Control.R
@@ -1,5 +1,24 @@
-load("RData/Traffic.Control.raw.RData")
-t <- sort(table(feature), decreasing=TRUE)
-par(las=2)
-par(mar=c(5,15,4,2))
-barplot(t, horiz=TRUE)
+process.Traffic.Control <- function(feature) {
+    #' Process Traffic.Controls
+    #'      keep top 3 cats, combine the rest into `Other`
+    #'
+    #' @param feature The raw Traffic.Controls from the data set
+    #' @return vector of string.
+    keep <- c("NO CONTROLS", "TRAFFIC SIGNAL", "STOP SIGN")
+    result <- vector(mode="character", length=length(feature))
+    for (i in 1:length(feature)) {
+        if (feature[i] %in% keep) {
+            result[i] = as.character(feature[i])
+        } else {
+            result[i] = "Other"
+        }
+    }
+    return(result)
+}
+
+if (!exists(".no.output")) {
+    load("RData/Traffic.Control.raw.RData")
+    feature <- process.Traffic.Control(feature)
+    save(feature, file="RData/Traffic.Control.RData")
+}
+
