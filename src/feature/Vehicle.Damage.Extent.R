707a18e9840eb5bef3eed3db3931ad9e5a0769f1
Donald Dong <xdong@csumb.edu>
2018-03-27 10:50:03 -0700

    processed Vehicle.Damage.Extent

--- src/feature/Vehicle.Damage.Extent.R
+++ src/feature/Vehicle.Damage.Extent.R
@@ -1,5 +1,24 @@
-load("RData/Vehicle.Damage.Extent.raw.RData")
-t <- sort(table(feature), decreasing=TRUE)
-par(las=2)
-par(mar=c(5,15,4,2))
-barplot(sort(table(feature), decreasing=TRUE), horiz=TRUE)
+process.Vehicle.Damage.Extent <- function(feature) {
+    #' Process Vehicle.Damage.Extents
+    #'     combine `OTHER`, `N/A`, `UNKNOWN` into `Other` 
+    #'
+    #' @param feature The raw Vehicle.Damage.Extents from the data set
+    #' @return vector of string.
+    combine <- c("OTHER", "N/A", "UNKNOWN")
+    result <- vector(mode="character", length=length(feature))
+    for (i in 1:length(feature)) {
+        if (!feature[i] %in% combine) {
+            result[i] = as.character(feature[i])
+        } else {
+            result[i] = "Other"
+        }
+    }
+    return(result)
+}
+
+if (!exists(".no.output")) {
+    load("RData/Vehicle.Damage.Extent.raw.RData")
+    feature <- process.Vehicle.Damage.Extent(feature)
+    save(feature, file="RData/Vehicle.Damage.Extent.RData")
+}
+
